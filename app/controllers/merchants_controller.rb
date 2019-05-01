b2f44c0f54258f6057a7b90cfb418220afc53889
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 15:54:46 -0700

    Add controller actions for Merchants, including create from Omniauth hash

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,84 @@
 class MerchantsController < ApplicationController
+  before_action :find_merchant, only: [:show, :edit, :update]
+  skip_before_action :require_login, only: [:create]
+
+  def index
+    @merchants = Merchant.all
+  end
+
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
+    if merchant
+      # Merchant was found in the database
+      flash[:success] = "Logged in as returning merchant #{merchant.name}"
+    else
+      # Merchant doesn't match anything in the DB
+      # Attempt to create a new merchant
+      merchant = Merchant.build_from_github(auth_hash)
+
+      if merchant.save
+        flash[:success] = "Logged in as new merchant #{merchant.name}"
+      else
+        # Couldn't save the merchant for some reason. If we
+        # hit this it probably means there's a bug with the
+        # way we've configured GitHub. Our strategy will
+        # be to display error messages to make future
+        # debugging easier.
+        flash[:error] = "Could not create new merchant account: #{merchant.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    # If we get here, we have a valid merchant instance
+    session[:merchant_id] = merchant.id
+    return redirect_to root_path
+  end
+
+  def current
+    @merchant = Merchant.find_by(id: session[:merchant_id])
+
+    unless @merchant
+      flash[:status] = :error
+      flash[:message] = "You must be logged in to see this page"
+      redirect_to login_path
+      return
+    end
+  end
+
+  def destroy # Destroy action = logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
+  end
+
+  # Show and edit are entirely handled by find_merchant helper method
+
+  def update
+    if @merchant.update(merchant_params)
+      flash[:status] = :success
+      flash[:message] = "Successfully updated merchant #{@merchant.id}"
+      redirect_to work_path(@work)
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not edit merchant #{@merchant.id}"
+      render :edit, status: :bad_request
+    end
+  end
+
+  private
+
+  def merchant_params
+    return params.require(:merchant).permit(:username, :email)
+  end
+    
+  def find_merchant
+    @merchant = Merchant.find_by(id: params[:id])
+    unless @merchant
+      head :not_found
+      return
+    end
+  end
 end
