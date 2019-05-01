fbca7f99100e34002b7bfd377f8ace86e1db033a
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 15:44:56 -0700

    Add login, create, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants, except: [:destroy]
+
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create"
+  delete "/logout", to: "merchants#destroy", as: "logout"
+
 end
