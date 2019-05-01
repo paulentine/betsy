36fa615197182036e745ccdb22ddec1a2531fbc1
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 11:13:29 -0700

    Setup Product's relationship to Merchant (belongs to)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_180454) do
+ActiveRecord::Schema.define(version: 2019_05_01_181019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 2019_05_01_180454) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_products_on_merchant_id"
   end
 
+  add_foreign_key "products", "merchants"
 end
