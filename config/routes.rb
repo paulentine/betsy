b1814a530cab178b98a6d27c7070e6fab52d8bf5
Riyo Perry <31839313+RPerry@users.noreply.github.com>
2019-05-01 16:31:49 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants, except: [:destroy]
+  resources :merchants, except: [:destroy, :edit, :update]
 
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create"
