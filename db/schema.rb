7c8fb871bc0bb7c321a3e1e0b0a9917052dec91f
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 11:22:25 -0700

    Create Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_181735) do
+ActiveRecord::Schema.define(version: 2019_05_01_182108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2019_05_01_181735) do
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
     t.index ["merchant_id"], name: "index_products_on_merchant_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "products", "merchants"
