fa716ed05de681e14d845b2d0dd145cbbcb4b2ab
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 11:26:49 -0700

    Setup review & product's relationship (reviews belong to product)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :merchant   # singular
   has_and_belongs_to_many :orders
+  has_many :reviews   # plural
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_182108) do
+ActiveRecord::Schema.define(version: 2019_05_01_182423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,10 @@ ActiveRecord::Schema.define(version: 2019_05_01_182108) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   add_foreign_key "products", "merchants"
+  add_foreign_key "reviews", "products"
 end
