a5845ced128e858c261e425155ac43021baf4d16
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 18:44:37 -0700

    Test Merchant's login & current action (success cases)

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "auth callback" do
+    it "can log in an existing user" do
+      # Arrange
+      merchant = merchants(:merchant1)
+
+       # Act
+      expect {
+        perform_login(merchant)
+      }.wont_change "Merchant.count"
+
+       # Assert
+      expect(session[:merchant_id]).must_equal merchant.id
+      must_redirect_to root_path
+    end
+  end
+
+  describe "current" do
+    it "responds with 302 Found for a logged-in merchant" do
+      # Arrange
+      perform_login
+
+      # Act
+      get current_merchant_path
+
+      # Assert
+      must_respond_with :found
+    end
+  end
 end
