8f34d2570ce960fdeb1820286d86b41daf0bf020
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-01 18:33:06 -0700

    Add path helper: auth_callback_path

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get "/orders/confirmation", to:"orders#confirmation", as:"order_confirmation"
   
   get "/auth/github", as: "github_login"
-  get "/auth/:provider/callback", to: "merchants#create"
+  get "/auth/:provider/callback", to: "merchants#create", as: "auth_callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
 
 end
