d7d77a79110f3ba410c3f3c86f8cdb3fc7e1d32c
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-02 13:48:21 -0700

    Change named path for login_path

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
   get "/orders/confirmation", to: "orders#confirmation", as: "order_confirmation"
 
-  get "/auth/github", as: "github_login"
-  get "/auth/:provider/callback", to: "merchants#create"
+  get "/auth/github", as: "login"
+  get "/auth/:provider/callback", to: "merchants#create", as: "callback"
   delete "/logout", to: "merchants#destroy", as: "logout"
 
   get "products/category/:id", to: "products#category", as: "category"
