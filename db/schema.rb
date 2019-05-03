26c95f65050f95533b0acf913f5c11b88d007156
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-03 16:31:19 -0700

    Add Status Column, default value: "pending"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_173925) do
+ActiveRecord::Schema.define(version: 2019_05_03_232819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_05_02_173925) do
     t.string "cc_num"
     t.string "cc_cvv"
     t.string "cc_expiration"
+    t.string "status", default: "pending"
   end
 
   create_table "products", force: :cascade do |t|
