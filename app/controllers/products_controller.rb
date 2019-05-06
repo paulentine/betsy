125f0de09e99d7f047579b268c52f31e41483c40
paulentine <45162016+paulentine@users.noreply.github.com>
2019-05-06 10:17:14 -0700

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -95,7 +95,7 @@ class ProductsController < ApplicationController
 
   def product_params
     puts "product_prams totall called"
-    return params.require(:product).permit(:name, :price, merchant_id: [], :description)
+    return params.require(:product).permit(:name, :price, :description, merchant_id: [])
   end
 
   def find_product
