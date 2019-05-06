ebef02502fd0ee6692cd3e79b2ee7f563854baa9
Riyo <riyo.perry@gmail.com>
2019-05-06 14:49:02 -0700

    fixed confirmation page. Everything works on the confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,20 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews, only: [:create]
   end
-  resources :orders
+  resources :orders do
+    get "confirmation", to: "orders#confirmation", as: "confirmation"
+  end
+
+  resources :orders do
+    get "order-items-for-order", to: "orders#order_items_order", as: "items_order"
+  end
 
   resources :merchants, except: [:destroy, :edit, :update] do
     resources :orders, only: [:index, :show]
   end
 
-  get "/orders/order-items-for-order", to: "orders#order_items_order", as: "order_items_order"
-  get "/orders/confirmation", to: "orders#confirmation", as: "order_confirmation"
+  get "merchant-orders-list", to: "orders#merchant_orders_list", as: "merchant_orders_list"
+
 
   get "/merchant/current", to: "merchants#current", as: "current_merchant"
 
