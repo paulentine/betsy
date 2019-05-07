e38c964e76968165548e99f3f543b8b5bdfca02d
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-07 10:49:01 -0700

    Clean up & reorganized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,38 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "homepages#index"
+  # Root
+  root 'homepages#index'
+
+  # Categories
+  resources :categories, except: [:destroy]
+
+  # Merchants
+  resources :merchants, except: %i[destroy edit update] do
+    resources :orders, only: %i[index show]
+  end
+
+  get 'merchant-orders-list', to: 'orders#merchant_orders_list', as: 'merchant_orders_list'
+
+  # Login-related
+  get '/merchant/current', to: 'merchants#current', as: 'current_merchant'
+  get '/auth/github', as: 'login'
+  get '/auth/:provider/callback', to: 'merchants#create', as: 'callback'
+  delete '/logout', to: 'merchants#destroy', as: 'logout'
+
+  # Orders
+  resources :orders do
+    get 'confirmation', to: 'orders#confirmation', as: 'confirmation'
+    get 'order-items-for-order', to: 'orders#order_items_order', as: 'items_order'
+  end
+
+  # Products
   resources :products do
-    resources :reviews, only: [:new, :create]
-  end
-  resources :orders do
-    get "confirmation", to: "orders#confirmation", as: "confirmation"
+    resources :reviews, only: %i[new create]
   end
 
-  resources :orders do
-    get "order-items-for-order", to: "orders#order_items_order", as: "items_order"
-  end
+  get 'products/category/:id', to: 'products#category', as: 'category'
 
-  resources :merchants, except: [:destroy, :edit, :update] do
-    resources :orders, only: [:index, :show]
-  end
-
-  get "merchant-orders-list", to: "orders#merchant_orders_list", as: "merchant_orders_list"
-
-
-  get "/merchant/current", to: "merchants#current", as: "current_merchant"
-
-  get "/auth/github", as: "login"
-  get "/auth/:provider/callback", to: "merchants#create", as: "callback"
-
-  delete "/logout", to: "merchants#destroy", as: "logout"
-
-  get "products/category/:id", to: "products#category", as: "category"
-  resources :categories, :except => [:destroy]
 end
