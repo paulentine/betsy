9d00721be8e8906881b2f07cb40a08062c2a0213
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-10 08:04:08 -0700

    Add checkout method, returns @order instance var

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
+  end
+
+  def checkout
+    @order = current_order
   end
 end
