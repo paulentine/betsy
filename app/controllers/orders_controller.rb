d10c65dc76ace830f92755f300edba45b9e4746e
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-10 08:05:28 -0700

    Write method to checkout order, updating with billing & shipping info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,52 +1,55 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
-    before_action :find_order, only: [:destroy, :order_items_order, :confirmation]
-    skip_before_action :require_login, only: [:new, :create, :confirmation, :order_items_order, :show]
-    
-    def index
-        if params[:merchant_id]
-    
-        merchant = Merchant.find_by(id: params[:merchant_id])
-            if merchant
-                @orders = merchant.orders
-            else
-                head :not_found
-                return
-            end
-        end
-    end
+  before_action :find_order, only: %i[destroy order_items_order confirmation]
+  skip_before_action :require_login, only: %i[new create confirmation order_items_order show]
 
-    def confirmation 
-    end
+  def index
+    if params[:merchant_id]
 
-    def order_items_order
-    end
-
-    def merchant_orders_list
-
-    end
-    
-    # Show is entirely the find_order helper
-     def show
-        @order = Order.find(params[:id])
-    
-        unless @order
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      if merchant
+        @orders = merchant.orders
+      else
         head :not_found
         return
         end
-     end
-    
-    private
-    
-    def order_params
-        return params.require(:order).permit(:email, :name, :address, :zipcode, :cc_num, :cc_cvv, :cc_expiration, :status)
     end
-    
-    def find_order
-        @order = Order.find(params[:order_id])
-    
-        unless @order
-        head :not_found
-        return
-        end
-    end  
+  end
+
+  def checkout
+    current_order.update(order_params)
+    redirect_to confirmation_path(current_order)
+  end
+
+  def confirmation; end
+
+  def order_items_order; end
+
+  def merchant_orders_list; end
+
+  # Show is entirely the find_order helper
+  def show
+    @order = Order.find(params[:id])
+
+    unless @order
+      head :not_found
+      return
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:email, :name, :address, :zipcode, :cc_num, :cc_cvv, :cc_expiration, :status)
+  end
+
+  def find_order
+    @order = Order.find(params[:order_id])
+
+    unless @order
+      head :not_found
+      return
+    end
+  end
 end
